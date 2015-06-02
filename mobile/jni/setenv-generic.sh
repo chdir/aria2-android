80940f6b12981a81b314663bec2930f8b2cf40c7
AlexanderR <alexander.r@gmx.com>
2015-06-02 23:32:47 +1000

    Do not build armv6 version: nobody seems to need it anymore

--- mobile/jni/setenv-generic.sh
+++ mobile/jni/setenv-generic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -e
 # Cross-compile environment for Android
 #####################################################################
 
--- mobile/jni/setenv-simple.sh
+++ mobile/jni/setenv-simple.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -e
 . ./setenv-generic.sh
 export CC="$ANDROID_TOOLCHAIN/${CROSS_COMPILE}gcc --sysroot=$SYSROOT"
 export CPP="$ANDROID_TOOLCHAIN/${CROSS_COMPILE}cpp --sysroot=$SYSROOT"
